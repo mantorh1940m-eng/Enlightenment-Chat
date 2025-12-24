c97e8886c0891d74b1280d732649b2fd19c2e80e
mantorh1940m-eng <mantorh1940m@gmail.com>
2025-12-24 09:38:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -152,3 +152,12 @@ messageInput.addEventListener("keydown", e => {
 ========================= */
 checkLogin();
 loadUsers();
+
+function toggleSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  if (sidebar.style.display === "block") {
+    sidebar.style.display = "none";
+  } else {
+    sidebar.style.display = "block";
+  }
+}
